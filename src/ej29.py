b696e50dfe63ad8db7443c41776024aa218227c4
irolram <rrivan@iesrafaelalberti.es>
2024-10-09 17:33:15 +0200

    28 y 29

---
+++ src/ej29.py
@@ -0,0 +1,26 @@
+#Cálculo de un número aleatorio entre dos valores
+
+import random
+
+def calculo_de_los_numero():
+if __name__ == "__main__":
+    main()
+
+"""
+def main():
+    
+    for i in range(1):
+        numa = (random.randint(1, 30))
+    
+
+    for i in range(1):
+        numb = (random.randint(1, 30))
+    
+
+    sumar = numa + numb
+    print(f"El primer numero: {numa} y el segundo numero: {numb} el total es: {sumar}")
+"""
+
+
+
+
