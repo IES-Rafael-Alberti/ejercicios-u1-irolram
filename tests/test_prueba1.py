77cf9fb68c7de037d1234a598cd8a29586735ab3
irolram <rrivan@iesrafaelalberti.es>
2024-10-16 15:35:55 +0200

    a

--- tests/test_prueba1.py
+++ tests/test_prueba1.py
@@ -1,5 +1,5 @@
 from src.prueba1 import retorna_mayor_numero
-import pytest
+
 
 def test_retornar_mayor_numero():
     assert retorna_mayor_numero (10,4) == 10
@@ -7,4 +7,3 @@ def test_retornar_mayor_numero():
     assert retorna_mayor_numero (0,0) == 0
     assert retorna_mayor_numero (0,1) == 1
 
-@pytest.mark.parametrize()
